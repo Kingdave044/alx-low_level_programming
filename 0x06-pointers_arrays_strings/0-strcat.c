582300d8fbc54e563c6612648bbf9023f1ce43f0
kingdave044 <kingdave4461@yahoo.com>
2022-09-22 19:57:31 -0700

    Low level programming

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,23 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _strcat - concatenates two strings
- * @dest: a string pointer
- * @src: another string pointer
- * Return: a string pointer
+ * _strcat - concatenates two strings.
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: concatnated string
  */
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest, src));
+	int len1 = 0, len2;
+
+	while (*(dest + len1) != '\0')
+		len1++;
+
+	for (len2 = 0; src[len2] != '\0'; ++len2)
+		dest[len1 + len2] = src[len2];
+
+	dest[len1 + len2] = '\0';
+
+	return (dest);
 }
