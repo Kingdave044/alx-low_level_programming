42a44dcb482aed9d5eb762cf9663d1b65b8b7284
kingdave044 <kingdave4461@yahoo.com>
2022-09-28 17:55:10 -0700

    Low level programming

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * sqrt_recursion - computes the square root of a number
+ * @n: integer number
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+int _sqrt(int n, int i)
+{
+	if (n < 0)
+		return (-1);
+
+	if ((i * i) > n)
+		return (-1);
+
+	if (i * i == n)
+		return (i);
+
+	return (_sqrt(n, ++i));
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,7 @@
+Recursion
+0- A function that prints a string followed by a new line
+1- A function that prints a string in reverse
+2- A function that returns the length of a string
+3- A function that returns the factorial of a given number
+4- A function that returns the value of x raised to the power of y
+5- A function that returns the square root of a number
