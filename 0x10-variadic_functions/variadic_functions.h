932d912b3c37aa738f3951e0053f532aba3d880f
kingdave044 <kingdave4461@yahoo.com>
2022-10-13 20:36:22 -0700

    Low level programming

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,23 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#define null (void *)0
+#define TRUE 1
+#define FALSE 0
+#define is ==
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct printer - A new struct type defining a printer
+ * @symbol: a symbol representing a data type
+ * @print: a function pointer to a function that prints a data
+ * 		type corresponding to a symbol
+ */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
